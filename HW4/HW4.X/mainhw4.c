321d3cd59142d979c7b53cc17946b475b2ebaa66
Deniseh595 <deniseheredia2017@u.northwestern.edu>
2017-04-16 20:29:41 -0500

    Finished HW 4 SPI

--- HW4/HW4.X/mainhw4.c
+++ HW4/HW4.X/mainhw4.c
@@ -12,32 +12,32 @@
 
 
 // DEVCFG0
-#pragma config DEBUG =  10 // no debugging
-#pragma config JTAGEN = 0 // no jtag
-#pragma config ICESEL = 11 // use PGED1 and PGEC1
-#pragma config PWP = 111111111  // no write protect
-#pragma config BWP =  1 // no boot write protect
-#pragma config CP = 1 // no code protect
+#pragma config DEBUG =  OFF // no debugging
+#pragma config JTAGEN = OFF // no jtag
+#pragma config ICESEL = ICS_PGx1 // use PGED1 and PGEC1
+#pragma config PWP = OFF  // no write protect
+#pragma config BWP =  OFF // no boot write protect
+#pragma config CP = OFF // no code protect
 
 // DEVCFG1
-#pragma config FNOSC = 011 // use primary oscillator with pll
-#pragma config FSOSCEN = 0 // turn off secondary oscillator
-#pragma config IESO = 0 // no switching clocks
-#pragma config POSCMOD = 10 // high speed crystal mode
-#pragma config OSCIOFNC = 1 // disable secondary osc
-#pragma config FPBDIV = 00 // divide sysclk freq by 1 for peripheral bus clock
-#pragma config FCKSM = 10 // do not enable clock switch
-#pragma config WDTPS =  10100 // use slowest wdt
-#pragma config WINDIS = 1 // wdt no window mode
-#pragma config FWDTEN = 0 // wdt disabled
-#pragma config FWDTWINSZ = 11 // wdt window at 25%
+#pragma config FNOSC = PRIPLL // use primary oscillator with pll
+#pragma config FSOSCEN = OFF // turn off secondary oscillator
+#pragma config IESO = OFF // no switching clocks
+#pragma config POSCMOD = HS // high speed crystal mode
+#pragma config OSCIOFNC = OFF // disable secondary osc
+#pragma config FPBDIV = DIV_1 // divide sysclk freq by 1 for peripheral bus clock
+#pragma config FCKSM = CSDCMD // do not enable clock switch
+#pragma config WDTPS =  PS1 // use slowest wdt
+#pragma config WINDIS = OFF // wdt no window mode
+#pragma config FWDTEN = OFF // wdt disabled
+#pragma config FWDTWINSZ = WINSZ_25 // wdt window at 25%
 
 // DEVCFG2 - get the sysclk clock to 48MHz from the 8MHz crystal
 #pragma config FPLLIDIV = DIV_2 // divide input clock to be in range 4-5MHz (/2)
 #pragma config FPLLMUL = MUL_24 // multiply clock after FPLLIDIV (*24 = 96))
 #pragma config FPLLODIV = DIV_2 // divide clock after FPLLMUL to get 48MHz (96/2)
 #pragma config UPLLIDIV = DIV_12// divider for the 8MHz input clock, then multiplied by 12 to get 48MHz for USB
-#pragma config UPLLEN =  0 // USB clock on
+#pragma config UPLLEN =  ON // USB clock on
 
 // DEVCFG3
 #pragma config USERID = 0000000000000000 // some 16bit userid, doesn't matter what
@@ -66,7 +66,7 @@ void initSPI1() {
   // setup spi1
   SPI1CON = 0;              // turn off the spi module and reset it
   SPI1BUF;                  // clear the rx buffer by reading from it
-  SPI1BRG = 0x1000;            // baud rate to 10 MHz [SPI1BRG = (80000000/(2*desired))-1]
+  SPI1BRG = 0x300;            // baud rate to 10 MHz [SPI1BRG = (80000000/(2*desired))-1]
   SPI1STATbits.SPIROV = 0;  // clear the overflow bit
   SPI1CONbits.CKE = 1;      // data changes when clock goes from hi to lo (since CKP is 0)
   SPI1CONbits.MSTEN = 1;    // master operation
@@ -91,7 +91,7 @@ void setVoltage(unsigned char channel, unsigned char voltage){
     unsigned char b1 = 0, b2 = 0; 
     
     b1=(channel <<7);
-    b1 = b1 | (0b1110000);
+    b1 = b1 | (0b01110000);
     b1 = b1 | (voltage >>4);
     
     b2 = voltage <<4; 
@@ -122,33 +122,32 @@ int main() {
 
     // do your TRIS and LAT commands here
     TRISBbits.TRISB4 = 1;       //set B4 as in
+    TRISAbits.TRISA4 = 0; 
     initSPI1();
             
     __builtin_enable_interrupts();
     
-    
-    _CP0_SET_COUNT(0);
+  
     int i = 0; 
     unsigned int sinwave[100];
     unsigned int rampwave[100];
     double temp;
    
-    
-    while (1) {
-      
-        for (i=0; i<100;i++){
+    for (i=0; i<100;i++){
         temp= 255.0/2.0+ 255.0/2.0*sin(3.1415*2.0*i/100.0);
         sinwave[i]= temp;
         rampwave [i]= i*255.0/100.0;
         }
         
-        int gettime;
-        gettime = _CP0_GET_COUNT();
-        setVoltage(0,sinwave[i]);
+    while (1) {  
+         _CP0_SET_COUNT(0);
+        for (i = 0; i<100; i++){
+        setVoltage(0,sinwave[i]); 
         setVoltage(1,rampwave[i]);
-        while ( _CP0_GET_COUNT() < 24000+gettime){
+        while ( _CP0_GET_COUNT() < 24000){
          ;
         }  //delay for right frequency 
+        }
     }
   return 0;  
 }
